5a6eddfcb9dfb9d166503477eb12db906d48096d
Pahihq1 <pashavinokurov2007yandex.ru>
2025-07-30 18:40:48 +0300

    feat: add priority support to Task class

--- src/task.js
+++ src/task.js
@@ -6,6 +6,7 @@ class Task {
         this.description = description;
         this.status = 'todo';
         this.createdAt = new Date();
+	this.priority = 'medium';
     }
 
     updateStatus(status) {
@@ -14,6 +15,13 @@ class Task {
             this.status = status;
         }
     }
+    setPriority(priority) {
+    const validPriorities = ['low', 'medium', 'high', 'urgent'];
+    if (validPriorities.includes(priority)) {
+        this.priority = priority;
+        return true;
+    }
+    return false;
 }
 
 module.exports = Task;
